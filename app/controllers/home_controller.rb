36a673c0215a24270e5d917c16dc0006765d74fb
Benjamin Lavena <benja@zmaster.com.ar>
2020-08-06 22:08:46 -0300

    mvp

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,87 @@
+class HomeController < ApplicationController
+  include HTTParty
+  def index
+  end
+  def search
+    if params[:q]==""
+      redirect_to root_path 
+    else
+      if params[:q] =~ /\d+\/(?:\d{4}|\d{2})/
+        login_url="https://login.justucuman.gov.ar/login"
+        response1 = HTTParty.get(login_url)
+        doc = Nokogiri::HTML(response1)
+        token=doc.at('[name="_token"]')["value"]
+
+        response2=HTTParty.post(
+          login_url,
+          multipart: true,
+          body: {
+            username: ENV["PJT_USERNAME"],
+            password: ENV["PJT_PASSWORD"],
+            _token: token
+          },
+          cookies: parse_cookies(response1)
+        )
+
+        response3=HTTParty.get(
+          "https://portaldelsae.justucuman.gov.ar/ingreso-escritos/create", 
+          cookies: parse_cookies(response2)
+          )
+        doc = Nokogiri::HTML(response3)
+        token=doc.at('[name="_token"]')["value"]
+
+        response4=HTTParty.post(
+          "https://portaldelsae.justucuman.gov.ar/ingreso-escritos/create/buscar-expediente",
+          cookies: parse_cookies(response3),
+          headers: {
+            "X-Requested-With": "XMLHttpRequest"
+          },
+          multipart: true,
+          body: {
+            e: params[:q],
+            f: '1',
+            _token: token
+          }
+        )
+        
+        response_body=JSON.parse response4.body
+
+        doc = Nokogiri::HTML(response_body["view"])
+        exptes = doc.css(".card.mb-3.border-secondary")
+
+        @response=exptes
+        @expedientes = []
+        exptes.each do |expte|
+          puts expte.at_css("strong").inner_text
+          @expedientes.push([expte.at_css("strong").inner_text,
+            expte.css("strong")[1].inner_text,
+            create_link(expte.at_css("a"))
+          ]
+          )
+        end
+
+      else
+        redirect_to root_path
+      end
+    end
+
+  end
+
+
+  private
+  def parse_cookies(response)
+    cookies=response.headers["set-cookie"]
+    parsed_cookies={}
+    cookies.split(",").each do |cookie|
+      if cookie.include? "pjtportalsae_session" or cookie.include? "XSRF-TOKEN" or cookie.include? "pagjudbeToken"
+        parsed_cookies[cookie.split("=")[0]]=cookie.split("=")[1]
+      end
+    end
+    parsed_cookies
+  end
+
+  def create_link(anchor)
+    last_bit=anchor["href"].scan(/.+(\/.+)$/).last[0]
+    anchor["href"].gsub("ingreso-escritos/create","expedientes").gsub(last_bit,"/historia")
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  root 'home#index'
+  post "/search", to: "home#search"
+  get "/search", to: "home#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
